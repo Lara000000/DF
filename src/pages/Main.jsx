6610cfbca1a5ba838c14c234f8794a2f5f8094d8
Lara000000 <xt01256xt@gmail.com>
2023-06-28 14:48:03 +0400

    Стили для Header

---
+++ src/pages/Main.jsx
@@ -0,0 +1,5 @@
+export function Main () {
+    return <>
+        <h1>Главная страница</h1>
+    </>
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+export {About} from "./About.jsx";
+export {AddProduct} from "./AddProduct.jsx";
+export {Auth} from "./Auth.jsx";
+export {Basket} from "./Basket.jsx";
+export {Delivery} from "./Delivery.jsx";
+export {Main} from "./Main.jsx";
+export {Products} from "./Products.jsx";
+export {Profile} from "./Profile.jsx";
+export {SingleProduct} from "./SingleProduct.jsx";
+
